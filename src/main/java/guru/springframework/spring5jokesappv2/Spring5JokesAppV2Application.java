5f477d92bbcdf7750b08acfa7856be5935d72e32
u19424099 <IVPiryutkina@sberbank.ru>
2021-10-20 13:03:27 +0300

    Created Spring Boot Project fix. Closes #3

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5JokesAppV2Application {
 
     public static void main(String[] args) {
-        int a=0;
+        int b=0;
         SpringApplication.run(Spring5JokesAppV2Application.class, args);
     }
 
